824a57a5bb9042d7c9c3ecafb665b8d34804bbc3
Isteyak <112750375+Isteyak12@users.noreply.github.com>
2023-12-01 23:57:17 -0500

    Update face_detector_streamlit.py

--- face_detector_streamlit.py
+++ face_detector_streamlit.py
@@ -11,26 +11,29 @@ def main():
     st.title("Face Detection with Streamlit")
 
     st.sidebar.header("Select Detection Method")
-    detection_method = st.sidebar.radio("Choose Detection Method", ("Image", "Live Video"))
+    detection_method = st.sidebar.radio("Choose Detection Method", ("Image", "Pre-recorded Video"))
 
     if detection_method == "Image":
         # Your image upload logic...
         pass
 
-    elif detection_method == "Live Video":
-        st.sidebar.subheader("Live Video Face Detection")
+    elif detection_method == "Pre-recorded Video":
+        st.sidebar.subheader("Pre-recorded Video Face Detection")
+        video_file = st.sidebar.file_uploader("Choose a video file...", type=["mp4"])  # Allow only MP4 videos
 
-        video_stream = cv2.VideoCapture(0)  # Try without specifying the index
-        stframe = st.empty()
+        if video_file is not None:
+            video_bytes = video_file.read()
+            cap = cv2.VideoCapture(video_bytes)
 
-        while True:
-            ret, frame = video_stream.read()
-            if not ret:
-                st.error("Failed to access the camera.")
-                break
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            result_img = detect_faces(frame)
-            stframe.image(result_img, caption='Live Video Face Detection', channels="RGB", use_column_width=True)
+            stframe = st.empty()
+
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                result_img = detect_faces(frame)
+                stframe.image(result_img, caption='Pre-recorded Video Face Detection', channels="RGB", use_column_width=True)
 
 if __name__ == '__main__':
     main()
